3431fe2d1305565c6089be2abbcaf6d3cb7ae76c
Rafael Guerra <rafael.guerra@valtech.com>
2024-07-08 20:25:23 -0300

    adding bash.sh

---
+++ Kubernetes/deploy_kubernetes_bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH=$PATH:/c/Windows/system32
+
+echo "Apply deployments e services"
+"$kubectl" apply -f api-deployment.yaml
+"$kubectl" apply -f api-svc.yaml
+
+echo "Apply scale objects (HPA)"
+"$kubectl" apply -f api-scaleobject.yaml
+
+echo "Os recursos do Kubernetes foram aplicados."
