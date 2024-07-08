013bf73b4ab0583bfb54e57a8678a4938e29790e
Rafael Guerra <rafael.guerra@valtech.com>
2024-07-08 20:24:20 -0300

    adding ps1 file

---
+++ Kubernetes/deploy_kubernetes_poweshell.ps1
@@ -0,0 +1,8 @@
+Write-Host "Apply deployments e services"
+kubectl apply -f api-deployment.yaml
+kubectl apply -f api-svc.yaml
+
+Write-Host "Apply scale objects (HPA)"
+kubectl apply -f api-scaleobject.yaml
+
+Write-Host "Os recursos do Kubernetes foram aplicados."
